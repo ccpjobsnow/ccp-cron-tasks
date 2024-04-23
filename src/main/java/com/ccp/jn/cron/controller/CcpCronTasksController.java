ce6f81d559fc0a7add32bb86a5e06fcbaa22667b
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-22 21:15:32 -0300

    ajustes em bulk e mensageria

--- src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
+++ src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
@@ -3,12 +3,12 @@ package com.ccp.jn.cron.controller;
 import java.util.function.Function;
 
 import com.ccp.decorators.CcpJsonRepresentation;
-import com.ccp.exceptions.process.CcpAsyncProcess;
+import com.ccp.exceptions.process.CcpAsyncTask;
 
 public class CcpCronTasksController {
 
 	public static void main(Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError, String taskName, String parameters) throws Exception {
 		CcpJsonRepresentation mdParameters = new CcpJsonRepresentation(parameters);
-		CcpAsyncProcess.executeProcess(taskName, mdParameters,jnAsyncBusinessNotifyError);
+		CcpAsyncTask.executeProcess(taskName, mdParameters,jnAsyncBusinessNotifyError);
 	}
 }
