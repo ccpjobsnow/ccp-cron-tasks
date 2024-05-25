197175f59796cefda48294a2c93270e97e610459
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-05-25 15:56:36 -0300

    desacoplamento entre sigla 'CCP' e elasticsearch, busca do 'union all ids' considerando registros com prazo de validade (disposable records)

--- src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
+++ src/main/java/com/ccp/jn/cron/controller/CcpCronTasksController.java
@@ -3,12 +3,11 @@ package com.ccp.jn.cron.controller;
 import java.util.function.Function;
 
 import com.ccp.decorators.CcpJsonRepresentation;
-import com.ccp.exceptions.process.CcpAsyncTask;
 
 public class CcpCronTasksController {
 
 	public static void main(Function<CcpJsonRepresentation, CcpJsonRepresentation> jnAsyncBusinessNotifyError, String taskName, String parameters) throws Exception {
-		CcpJsonRepresentation mdParameters = new CcpJsonRepresentation(parameters);
-		CcpAsyncTask.executeProcess(taskName, mdParameters,jnAsyncBusinessNotifyError);
+//		CcpJsonRepresentation mdParameters = new CcpJsonRepresentation(parameters);
+//		CcpAsyncTask.executeProcess(taskName, mdParameters,jnAsyncBusinessNotifyError);
 	}
 }
